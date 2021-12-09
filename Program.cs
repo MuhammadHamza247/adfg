7d155c9ae0b8df36e6514c8b09439faf7b8d7159
Muhammad Hamza <ghulamabbas2247@gmail.com>
2021-12-09 22:35:05 +0500

    asdf

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classfundamental
+{
+        public class Person
+        {
+            public string FirstName;
+            public string LastName;
+            public void introduced()
+             {
+            Console.WriteLine("Hi my name is "+ FirstName +" "+ LastName + ".");
+             }
+        }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Person per = new Person();
+        per.FirstName ="Muhammad";
+        per.LastName ="Hamza";
+        per.introduced();
+        }
+    }
+        
+}
